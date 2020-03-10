25d287c494ef3e4e0b16115e4e2ca97e2296a657
1328286113 <1328286113@qq.com>
2020-03-10 23:40:35 +0800

    home

--- 16-layout/src/main/java/com/example/a16_layout/view/TagLayout.kt
+++ 16-layout/src/main/java/com/example/a16_layout/view/TagLayout.kt
@@ -1,16 +1,67 @@
 package com.example.a16_layout.view
 
 import android.content.Context
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.ViewGroup
+import kotlin.math.max
 
 class TagLayout(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+    var childbounds = ArrayList<Rect>()
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-
+        var widthUsed = 0
+        var heightUsed = 0
+        var lineWidthUsed = 0
+        var lineHeightUsed = 0
+        var widthSpecMode = MeasureSpec.getMode(widthMeasureSpec)
+        var widthSpecSize = MeasureSpec.getSize(widthMeasureSpec)
+        for (i in 0 until childCount - 1) {
+            var childview = getChildAt(i)
+            measureChildWithMargins(childview, widthMeasureSpec, 0, heightMeasureSpec, heightUsed)
+            var childbound: Rect
+            if (childbounds.size <= i) {
+                childbound = Rect()
+                childbounds.add(childbound)
+            } else {
+                childbound = childbounds[i]
+            }
+            if (widthSpecMode != MeasureSpec.UNSPECIFIED &&
+                childview.measuredWidth + lineWidthUsed > widthSpecSize) {
+                lineWidthUsed = 0
+                heightUsed += lineHeightUsed
+                lineHeightUsed = 0
+//                measureChildWithMargins(childview, widthMeasureSpec, 0, heightMeasureSpec, heightUsed)
+            }
+            childbound.set(
+                lineWidthUsed,
+                heightUsed,
+                lineWidthUsed + childview.measuredWidth,
+                heightUsed + childview.measuredHeight
+            )
+            lineWidthUsed += childview.measuredWidth
+            widthUsed = max(widthUsed,lineWidthUsed)
+            lineHeightUsed = max(lineHeightUsed, childview.measuredHeight)
+        }
+        var width = widthUsed
+        var height = heightUsed
+        setMeasuredDimension(width, height)
     }
-    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
 
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        for (i in 0 until childCount - 1) {
+            var childbound = childbounds[i]
+            getChildAt(i).layout(
+                childbound.left,
+                childbound.top,
+                childbound.right,
+                childbound.bottom
+            )
+        }
+    }
+
+    override fun generateLayoutParams(attrs: AttributeSet?): LayoutParams {
+        return MarginLayoutParams(context, attrs)
     }
 }
